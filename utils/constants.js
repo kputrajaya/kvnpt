b5a9a7f9164441e8c42dbb90300fe62fce9edb43
kiloev <freakperzon@yahoo.com>
2021-04-27 19:46:00 +0700

    Set default token to empty string

--- utils/constants.js
+++ utils/constants.js
@@ -6,4 +6,4 @@ export const BLOG_PER_PAGE_MAX = 100
 
 export const IMAGEKIT_ENDPOINT = process.env.IMAGEKIT_ENDPOINT || 'https://ik.imagekit.io/kvn/'
 
-export const STORYBLOK_TOKEN = process.env.STORYBLOK_TOKEN
+export const STORYBLOK_TOKEN = process.env.STORYBLOK_TOKEN || ''
