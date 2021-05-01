4e89181b4055e6bd0c1f20b046d30eec60fd6a9f
kiloev <freakperzon@yahoo.com>
2021-05-01 22:58:07 +0700

    Fix no flash

--- pages/_document.js
+++ pages/_document.js
@@ -1,37 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
+  noFlash = function () {
+    var key = 'darkMode';
+    var clsDark = 'dark';
+    var clsLight = 'light';
+    var elm = document.documentElement;
+
+    var useDark;
+    try {
+      useDark = JSON.parse(localStorage.getItem(key));
+    } catch (err) {
+      useDark = window.matchMedia
+        ? window.matchMedia('(prefers-color-scheme: dark)').matches
+        : elm.classList.contains(clsDark);
+      try {
+        localStorage.setItem(key, JSON.stringify(useDark));
+      } catch (err) { }
+    }
+
+    elm.classList.add(useDark ? clsDark : clsLight);
+    elm.classList.remove(useDark ? clsLight : clsDark);
+  }
+
   render() {
     return (
       <Html>
         <Head />
         <body>
-          <script>
-            {`
-              // Prevent dark mode flash
-              (function() {
-                var key = 'darkMode';
-                var clsDark = 'dark';
-                var clsLight = 'light';
-                var elm = document.documentElement;
-
-                var useDark;
-                try {
-                  useDark = JSON.parse(localStorage.getItem(key));
-                } catch (err) {
-                  useDark = window.matchMedia
-                    ? window.matchMedia('(prefers-color-scheme: dark)').matches
-                    : elm.classList.contains(clsDark);
-                  try {
-                    localStorage.setItem(key, JSON.stringify(useDark));
-                  } catch (err) { }
-                }
-
-                elm.classList.add(useDark ? clsDark : clsLight);
-                elm.classList.remove(useDark ? clsLight : clsDark);
-              })();
-            `}
-          </script>
+          <script dangerouslySetInnerHTML={{__html: `(${this.noFlash.toString()})();`}} />
           <Main />
           <NextScript />
         </body>
