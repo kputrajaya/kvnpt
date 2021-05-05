8d0583354231dddc2dd85340bc87a0b07ff13a4e
kiloev <freakperzon@yahoo.com>
2021-05-06 00:10:59 +0700

    Add background

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,21 @@ module.exports = {
       },
       colors: {
         gray: colors.trueGray,
-      }
+      },
+      backgroundImage: (theme) => ({
+        'custom-dark': `linear-gradient(to bottom, rgba(23, 23, 23, 0) 40%, rgba(23, 23, 23, 1) 60%), url('/images/background-dark.jpg')`,
+        'custom-light': `linear-gradient(to bottom, rgba(245, 245, 245, 0) 40%, rgba(245, 245, 245, 1) 60%), url('/images/background-light.jpg')`,
+      }),
+      zIndex: {
+        '-1': '-1',
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ['dark'],
+      opacity: ['dark', 'responsive'],
+    },
   },
   plugins: [],
 }
