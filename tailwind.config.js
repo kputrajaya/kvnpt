a85d6cfd502a1f3bc11e434f7b7feea79f03eab4
kiloev <freakperzon@yahoo.com>
2021-05-07 00:39:07 +0700

    Try prefixing background img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 const colors = require('tailwindcss/colors')
 
+const imagePrefix = process.env.HOSTNAME && process.env.IMAGEKIT_ENDPOINT
+  ? (process.env.IMAGEKIT_ENDPOINT + process.env.HOSTNAME)
+  : ''
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -17,8 +21,8 @@ module.exports = {
         },
       },
       backgroundImage: (theme) => ({
-        'custom-dark': `linear-gradient(to bottom, rgba(23, 23, 23, .2) 40%, rgba(23, 23, 23, 1) 60%), url('/images/background-dark.jpg')`,
-        'custom-light': `linear-gradient(to bottom, rgba(245, 245, 245, .2) 40%, rgba(245, 245, 245, 1) 60%), url('/images/background-light.jpg')`,
+        'custom-dark': `linear-gradient(to bottom, rgba(23, 23, 23, .2) 40%, rgba(23, 23, 23, 1) 60%), url('${imagePrefix}/images/background-dark.jpg')`,
+        'custom-light': `linear-gradient(to bottom, rgba(245, 245, 245, .2) 40%, rgba(245, 245, 245, 1) 60%), url('${imagePrefix}/images/background-light.jpg')`,
       }),
       zIndex: {
         '-1': '-1',
--- utils/constants.js
+++ utils/constants.js
@@ -6,7 +6,9 @@ export const BLOCK_PHOTO_MAX_WIDTH = 704
 export const BLOG_PER_PAGE = 20
 export const BLOG_PER_PAGE_MAX = 100
 
-export const IMAGEKIT_ENDPOINT = 'https://ik.imagekit.io/kvn/'
+export const HOSTNAME = process.env.HOSTNAME || ''
+
+export const IMAGEKIT_ENDPOINT = process.env.IMAGEKIT_ENDPOINT || ''
 
 export const INTRO_AVATAR_SIZE = 125
 export const INTRO_CONTACT_SIZE = 25
