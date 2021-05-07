bd5b6cbd1c6ade269068e85b36d03c6122754dea
kiloev <freakperzon@yahoo.com>
2021-05-07 07:27:49 +0700

    Shorten revalidate to 10 seconds

--- utils/constants.js
+++ utils/constants.js
@@ -15,6 +15,6 @@ export const INTRO_CONTACT_SIZE = 25
 
 export const RESUME_AVATAR_SIZE = 125
 
-export const STATIC_PROPS_REVALIDATE = process.env.STATIC_PROPS_REVALIDATE || 60
+export const STATIC_PROPS_REVALIDATE = process.env.STATIC_PROPS_REVALIDATE || 10
 
 export const STORYBLOK_TOKEN = process.env.STORYBLOK_TOKEN || ''
