fc96cc70de30d1dc7e033a02e2374a4f7f213fb5
kiloev <freakperzon@yahoo.com>
2021-05-13 11:11:35 +0700

    Photo album link display date

--- components/block_photo_album_link.js
+++ components/block_photo_album_link.js
@@ -1,17 +1,17 @@
 import {BLOCK_PHOTO_ALBUM_LINK_PREVIEW_COUNT} from '../utils/constants'
 import PhotoAlbumImages from './photo_album_images'
+import PostSubtitle from './post_subtitle'
 
 export default function BlockPhotoAlbumLink({block}) {
   return (
     <div className="kvn-card">
       <div className="mb-4">
-        <div className="text-lg font-semibold">
+        <div className="mb-1 text-lg font-semibold">
           <a href={`/photos/${block.photo_album.slug}`}>{block.photo_album.name}</a>
         </div>
-        {
-          block.photo_album.content.description &&
-          <div className="text-sm text-scheme-third">{block.photo_album.content.description}</div>
-        }
+        <div className="text-sm text-scheme-third">
+          <PostSubtitle date={block.photo_album.content.date} />
+        </div>
       </div>
 
       <PhotoAlbumImages album={block.photo_album} previewCount={BLOCK_PHOTO_ALBUM_LINK_PREVIEW_COUNT} />
