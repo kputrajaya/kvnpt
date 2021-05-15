c7d03cd9f8b22426f2abeed3602c48b06b6b75db
Kevin Putrajaya <>
2021-05-15 11:35:41 +0700

    Comments page

---
+++ pages/comments.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+import BackButton from '../../components/back_button'
+import CommentsInner from '../../components/comments'
+
+export default function Comments({albums}) {
+  return (
+    <>
+      <Head>
+        <title>Comments - {SITE_TITLE}</title>
+      </Head>
+
+      <BackButton href="/" />
+      <h1 className="mb-8 text-2xl font-semibold">Comments</h1>
+
+      <CommentsInner />
+    </>
+  )
+}
