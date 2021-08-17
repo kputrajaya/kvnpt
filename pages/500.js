dcbdee2450e3c66975ef762346d721c09770a3b0
kiloev <freakperzon@yahoo.com>
2021-08-17 08:55:42 +0700

    Update error message wording

--- pages/500.js
+++ pages/500.js
@@ -7,13 +7,13 @@ export default function Custom500() {
   return (
     <>
       <Head>
-        <title>Server Error - {SITE_TITLE}</title>
+        <title>Internal Server Error - {SITE_TITLE}</title>
       </Head>
 
       <div className="h-screen -my-8 py-8 flex items-center justify-center">
         <div className="text-center">
           <div className="text-5xl font-semibold">500</div>
-          <h1 className="my-4 text-lg">There was an internal server error.</h1>
+          <h1 className="my-4 text-lg">Something went wrong.</h1>
           <BackButton href="/" />
         </div>
       </div>
