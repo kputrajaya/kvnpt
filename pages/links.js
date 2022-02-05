f8908f08334d08431ecf9587d11cea7d0e9a447d
kputrajaya <freakperzon@yahoo.com>
2022-02-05 17:11:55 +0700

    Open in new tab

--- pages/links.js
+++ pages/links.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
 
 import { SITE_TITLE, STATIC_PROPS_REVALIDATE } from '../utils/constants';
 import BackButton from '../components/back_button';
@@ -16,12 +15,10 @@ export default function Links({ links }) {
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {links.map((link, index) => (
-          <Link href={`/${link.short}`} key={index}>
-            <a className="kvn-card block">
-              <h2 className="mb-1 font-semibold">/{link.short}</h2>
-              <h3 className="text-sm text-scheme-third">{link.name}</h3>
-            </a>
-          </Link>
+          <a className="kvn-card block" href={`/${link.short}`} target="_blank" rel="noreferrer" key={index}>
+            <h2 className="mb-1 font-semibold">/{link.short}</h2>
+            <h3 className="text-sm text-scheme-third">{link.name}</h3>
+          </a>
         ))}
       </div>
     </>
