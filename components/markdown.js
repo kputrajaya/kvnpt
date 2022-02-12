9e0ea6dfc4b667ffb87a74d94c527b95e7a300dc
kputrajaya <freakperzon@yahoo.com>
2022-02-12 22:07:26 +0700

    Revert theme to coldark-cold and tomorrow

--- components/markdown.js
+++ components/markdown.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 const SyntaxHighlighter = dynamic(() => import('react-syntax-highlighter').then((mod) => mod.PrismAsyncLight), {
   ssr: false,
 });
-import { vs, vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { coldarkCold, tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import { useDark } from '../utils/generics';
 
@@ -26,7 +26,7 @@ export default function Markdown({ text }) {
     code({ node, inline, className, children, ...props }) {
       const match = /language-(\w+)/.exec(className || '');
       return !inline && match ? (
-        <SyntaxHighlighter style={darkMode.value ? vscDarkPlus : vs} language={match[1]} PreTag="div" {...props}>
+        <SyntaxHighlighter style={darkMode.value ? tomorrow : coldarkCold} language={match[1]} PreTag="div" {...props}>
           {String(children).replace(/\n$/, '')}
         </SyntaxHighlighter>
       ) : (
