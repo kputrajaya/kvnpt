4feacb5bf17ef54e62efe92f9a1cad8362d65810
kputrajaya <freakperzon@yahoo.com>
2023-01-29 20:51:45 +0700

    404 wording

--- pages/404.tsx
+++ pages/404.tsx
@@ -13,7 +13,7 @@ export default function Custom404() {
       <div className="-my-8 flex h-screen items-center justify-center py-8">
         <div className="text-center">
           <div className="text-5xl font-semibold">404</div>
-          <h1 className="my-4 text-lg">Something went wrong.</h1>
+          <h1 className="my-4 text-lg">Page does not exist.</h1>
           <BackButton href="/" />
         </div>
       </div>
