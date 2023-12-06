451148c7fe98cf84e5704e7b39fc3c9dac5043bb
Kevin Putrajaya <freakperzon@yahoo.com>
2023-12-06 23:57:15 +0700

    Fix block album link

--- components/block_photo_album_link.tsx
+++ components/block_photo_album_link.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link';
 
 import { BLOCK_PHOTO_ALBUM_LINK_PREVIEW_COUNT } from '../utils/constants';
+import { PHOTO_ALBUMS } from '../utils/contents';
 import PhotoAlbumImages from './photo_album_images';
 import PostSubtitle from './post_subtitle';
 
 export default function BlockPhotoAlbumLink({ block }) {
+  const album = PHOTO_ALBUMS.find((album) => album.slug === block.slug);
+
   return (
     <div className="kvn-card">
       <div className="mb-4">
         <div className="mb-1 text-lg font-semibold">
-          <Link href={`/photos/${block.photo_album.slug}`}>{block.photo_album.name}</Link>
+          <Link href={`/photos/${album.slug}`}>{album.name}</Link>
         </div>
         <div className="text-scheme-third text-sm">
-          <PostSubtitle date={block.photo_album.content.date} />
+          <PostSubtitle date={album.date} />
         </div>
       </div>
 
-      <PhotoAlbumImages album={block.photo_album} previewCount={BLOCK_PHOTO_ALBUM_LINK_PREVIEW_COUNT} />
+      <PhotoAlbumImages album={album} previewCount={BLOCK_PHOTO_ALBUM_LINK_PREVIEW_COUNT} />
     </div>
   );
 }
