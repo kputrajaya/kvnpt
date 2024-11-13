f9723ef8b66fac1080993fdbd0fcb19fcf257073
Kevin Putrajaya <freakperzon@yahoo.com>
2024-11-13 08:42:29 +0700

    Add Masa Sepi

--- utils/contents.tsx
+++ utils/contents.tsx
@@ -434,6 +434,18 @@ export const SONG_ALBUMS = [
     ],
   },
   {
+    name: 'Mini Projects',
+    link: '',
+    songs: [
+      {
+        audio: 'https://d.kvn.pt/songs/masa_sepi.mp3',
+        title: 'Masa Sepi (Bernadya)',
+        duration: '00:31',
+        featuring: '',
+      },
+    ],
+  },
+  {
     name: 'ViCiDi',
     link: '',
     songs: [
