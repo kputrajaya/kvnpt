6ae3ac50503297a2ff1c136db43ff4cd7157463a
Kevin Putrajaya <freakperzon@yahoo.com>
2025-12-07 10:50:47 +0700

    Update import

--- utils/dates.tsx
+++ utils/dates.tsx
@@ -1,5 +1,5 @@
-import format from 'date-fns/format';
-import parseISO from 'date-fns/parseISO';
+import { format } from 'date-fns/format';
+import { parseISO } from 'date-fns/parseISO';
 
 export const formatDate = (date) => {
   const dateWithoutZ = date.substr(-1) === 'Z' ? date.substr(0, date.length - 1) : date;
